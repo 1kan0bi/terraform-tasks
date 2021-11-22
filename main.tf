c07a3a1e1c89d7e4da0827ca70802638f51d2cc3
kanobi <bulutovstas@mail.ru>
2021-11-22 16:33:45 +0300

    second

--- main.tf
+++ main.tf
@@ -16,15 +16,18 @@ resource "digitalocean_tag" "ter02" {
 }
 resource "digitalocean_droplet" "ter02" {
   image    = "ubuntu-18-04-x64"
-  name     = "ter_task_02"
-  region   = "nyc2"
+  name     = "ter-task-02"
+  region   = "ams3"
   size     = "s-1vcpu-1gb"
   tags     = [digitalocean_tag.ter02.name]
-  ssh_keys = ["${digitalocean_ssh_key.ter02.public_key}"]
+  ssh_keys = ["${digitalocean_ssh_key.my_ssh_key.fingerprint}", "${data.digitalocean_ssh_key.ter02.fingerprint}"]
 }
 
+data "digitalocean_ssh_key" "ter02" {
+  name       = "REBRAIN.SSH.PUB.KEY"
+ }
 
-resource "digitalocean_ssh_key" "ter02" {
+resource "digitalocean_ssh_key" "my_ssh_key" {
   name       = "my ssh key"
   public_key = var.ssh_key
 }
